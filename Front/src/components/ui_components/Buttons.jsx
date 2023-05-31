d47293f279a46eaa3d4c4038c4e28f491efa36ee
dkgmgo <dkgm12@yahoo.com>
2023-05-31 11:41:50 +0200

    design system ongoing

---
+++ Front/src/components/ui_components/Buttons.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import '../css/Buttons.css'
+
+class ButtonDefault extends Component {
+    constructor(props) {
+        super(props)
+        this.handleClick.bind(this)
+    }
+
+    handleClick(e) {
+        console.log("click")
+    }
+
+    render() {
+        const { color, icon, circular, rounded, style, children } = this.props
+
+        let btnClasses = "btn btn-plain btn-" + color
+        let btnStyle = circular ? " circle-btn" : ""
+        btnStyle += rounded ? " rounded-btn" : ""
+
+        return (
+            <div className={btnStyle}>
+                <button className={btnClasses} onClick={this.handleClick} style={style}>
+                    <i className={icon}></i>
+                    {children}
+                </button>
+            </div>
+        )
+    }
+}
+
+export function ButtonPrimary({ icon, style, children }) {
+    return (
+        <ButtonDefault color="primary" icon={icon} style={style} children={children} />
+    )
+}
+
+export function CircularButton({ color, icon, style, children }) {
+    return (
+        <ButtonDefault color={color} icon={icon} circular={true} style={style} children={children} />
+    )
+}
+
+export function RoundedButton({ color, icon, style, children }) {
+    return (
+        <ButtonDefault color={color} icon={icon} rounded={true} style={style} children={children} />
+    )
+}
--- Front/src/views/DesignSystem.jsx
+++ Front/src/views/DesignSystem.jsx
@@ -1,28 +1,40 @@
 import React, { Component } from 'react'
 import { BasicLineChart, StackedAreaChart, SmoothedLineChart, SmoothedAreaChart, BasicAreaChart } from '../components/charts/Lines'
+import { ButtonPrimary, CircularButton, RoundedButton } from '../components/ui_components/Buttons'
 
 class DesignSystem extends Component {
     render() {
         return (
             <div>
                 <nav className='navbar navbar-dark bg-dark d-flex flex-column'>
-                    <p className="text-white text-center h2 mt-2">Bienvenu sur le pseudo design system du site</p>
+                    <p className="text-white text-center h2 mt-2">Bienvenue sur le pseudo design system du site</p>
                     <p className='text-white h6 text-justify'>Ce site est principalement fait pour les développeurs et il regroupe l'ensemble de composants du site pour standardiser les développements</p>
                 </nav>
                 <ul>
-                    <li>
+                    <li className='mt-5'>
                         <div className='mt-2'>
                             <span>Buttons</span>
                             <p>Voici les boutons utilisables sur le site</p>
+                            <div className='mt-2 d-flex flex-row justify-content-around'>
+                                <div>
+                                    <ButtonPrimary>Bouton</ButtonPrimary>
+                                </div>
+                                <div>
+                                    <RoundedButton color="warning" icon="fa fa-home" />
+                                </div>
+                                <div>
+                                    <CircularButton color="primary" icon="fa fa-camera fa-2x" />
+                                </div>
+                            </div>
                         </div>
                     </li>
-                    <li>
+                    <li className='mt-5'>
                         <div className='mt-2'>
                             <span>Forms</span>
                             <p>Voici les formulaires utilisables sur le site</p>
                         </div>
                     </li>
-                    <li>
+                    <li className='mt-5'>
                         <div className='mt-2'>
                             <span>Lines Charts</span>
                             <p>Voici les Graphiques de lignes, la plupart des lignes héritent de StackedAreaChart</p>
