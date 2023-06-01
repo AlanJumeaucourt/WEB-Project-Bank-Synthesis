484be0c7ba81f0a316d7ecae0985f97b3f3f7ce0
AlanJumeaucourt <122749528+AlanJumeaucourt@users.noreply.github.com>
2023-06-01 10:01:41 +0000

    Add cumulative balance

--- Back/app/Endpoints/accounts.py
+++ Back/app/Endpoints/accounts.py
@@ -1,6 +1,7 @@
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 import mariadb
+from datetime import datetime, timedelta
 
 def list_accounts():
     try:
@@ -26,3 +27,59 @@ def list_accounts():
         print(f"Erreur lors de la récupération des comptes : {e}")
         return JSONResponse(content={"message": "Erreur lors de la récupération des comptes"}, status_code=500)
 
+
+def get_account_balance(compte_id: int):
+    try:
+        # Connexion à la base de données MariaDB
+        conn = mariadb.connect(
+            user="root",
+            password="rootpassword",
+            host="mariadb",
+            port=3306,
+            database="User1"
+        )
+
+        cursor = conn.cursor()
+        cursor.execute("SELECT SUM(amount) FROM Transactions WHERE source_account_id = ? OR destination_account_id = ?", (compte_id, compte_id))
+        balance = cursor.fetchone()[0]
+        cursor.close()
+
+        if balance is None:
+            return JSONResponse(content={"message": "Compte introuvable"}, status_code=404)
+
+        return JSONResponse(content={"solde": balance})
+    except mariadb.Error as e:
+        print(f"Erreur lors de la récupération du solde du compte : {e}")
+        return JSONResponse(content={"message": "Erreur lors de la récupération du solde du compte"}, status_code=500)
+
+def get_account_balance_cumulative(compte_id: int):
+    try:
+        # Connexion à la base de données MariaDB
+        conn = mariadb.connect(
+            user="root",
+            password="rootpassword",
+            host="mariadb",
+            port=3306,
+            database="User1"
+        )
+        start_date = datetime(2021, 1, 1)
+        end_date = datetime(2021, 12, 31)
+
+        cursor = conn.cursor()
+        balances_per_day = {}
+
+        current_date = start_date
+        while current_date <= end_date:
+            cursor.execute("SELECT COALESCE(SUM(amount), 0) FROM Transactions WHERE (source_account_id = ? OR destination_account_id = ?) AND transaction_date <= ?", (compte_id, compte_id, current_date))
+            balance = cursor.fetchone()[0]
+
+            balances_per_day[current_date.strftime("%Y-%m-%d")] = balance
+
+            current_date += timedelta(days=1)
+
+        cursor.close()
+
+        return JSONResponse(content=balances_per_day)
+    except mariadb.Error as e:
+        print(f"Erreur lors de la récupération des soldes par jour : {e}")
+        return JSONResponse(content={"message": "Erreur lors de la récupération des soldes par jour"}, status_code=500)
--- Back/app/main.py
+++ Back/app/main.py
@@ -3,7 +3,9 @@ from typing import Union
 from fastapi import FastAPI
 
 from app.Endpoints.hello import hello as hello_from_api
-from app.Endpoints.accounts import list_accounts as list_accounts
+from app.Endpoints.accounts import list_accounts
+from app.Endpoints.accounts import get_account_balance
+from app.Endpoints.accounts import get_account_balance_cumulative
 
 app = FastAPI()
 
@@ -17,8 +19,17 @@ async def get_accounts():
     return list_accounts()
 
 
+
+@app.get("/comptes/{compte_id}/solde")
+async def comptesSolde(compte_id: int):
+    return get_account_balance(compte_id)
+
+
+@app.get("/comptes/{compte_id}/soldeperiode")
+async def comptesSoldecommule(compte_id: int):
+    return get_account_balance_cumulative(compte_id)
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
-
 
