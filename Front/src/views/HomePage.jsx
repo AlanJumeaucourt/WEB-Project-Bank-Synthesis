77fc85c9d65a4292bd4f0ffcc1c7a11993461306
Zouzzou21 <lucas.landy@orange.fr>
2023-06-08 22:49:16 +0200

    First push HomePage (WIP)

--- Front/src/views/HomePage.jsx
+++ Front/src/views/HomePage.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
+import { ButtonPrimary } from '../components/ui_components/Buttons'
 
 class HomePage extends Component {
     render() {
         return (
             <div>
-                <h1>TCArgent HomePage</h1>
+                <div className='mt-2'>
+                    <h1>TCArgent HomePage</h1>
+                </div>
+                <a href="https://localhost:3000/Sigin" target="_blanck">
+                    <ButtonPrimary name = "Sign In" color="primary" icon="fa fa-sign-in" />
+                </a>
+                <a href="https://localhost:3000/Login" target="_blanck">
+                    <ButtonPrimary color="primary" icon="fa fa-address-book" />
+                </a>
             </div>
         )
     }
