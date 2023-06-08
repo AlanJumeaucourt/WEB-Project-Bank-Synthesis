3b055c5a4895687510c6060c27dcbb3b18befd46
alan jumeaucourt <alan.jumeaucourt@gmail.com>
2023-06-08 09:28:35 +0200

    Add endpoints and fix Python acces to mariadb

--- Back/app/main.py
+++ Back/app/main.py
@@ -1,7 +1,11 @@
 from typing import Union
 
 from fastapi import FastAPI
+
 from app.Endpoints.hello import hello as hello_from_api
+from app.Endpoints.accounts import list_accounts
+from app.Endpoints.accounts import get_account_balance
+from app.Endpoints.accounts import get_account_balance_cumulative
 
 app = FastAPI()
 
@@ -10,6 +14,20 @@ app = FastAPI()
 def read_root():
     return hello_from_api()
 
+@app.get("/comptes")
+async def get_accounts():
+    return list_accounts()
+
+
+
+@app.get("/comptes/{compte_id}/solde")
+async def comptesSolde(compte_id: int):
+    return get_account_balance(compte_id)
+
+
+@app.get("/comptes/{compte_id}/soldeperiode")
+async def comptesSoldecommule(compte_id: int):
+    return get_account_balance_cumulative(compte_id)
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
--- Front/src/views/Patrimoine.jsx
+++ Front/src/views/Patrimoine.jsx
@@ -25,6 +25,7 @@ const HomePage = () => {
   
       fetchDataFromAPI();
     }, []);
+}
 
 
 class Patrimoine extends Component {
