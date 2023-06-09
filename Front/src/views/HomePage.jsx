22c58393698ea8cb55ddf99afa7baf08572ee079
Zouzzou21 <lucas.landy@orange.fr>
2023-06-09 16:16:06 +0200

    HomePage update dirty but fonctionnal

--- Front/src/views/HomePage.jsx
+++ Front/src/views/HomePage.jsx
@@ -1,22 +1,62 @@
-import React, { Component } from 'react'
-import { ButtonPrimary } from '../components/ui_components/Buttons'
+import React, { Children, Component } from 'react'
+import { useKeycloak } from "@react-keycloak/web";
+import { ButtonPrimary} from '../components/ui_components/Buttons'
+import { Drawer } from '../components/ui_components/Drawer'
 
-class HomePage extends Component {
-    render() {
+export function HomePage() {
+    const { keycloak } = useKeycloak()
+    const login = () => {
+        keycloak.login()
+    }
+    const logout = () => {
+        keycloak.logout()
+    }
+    const register = () => {
+        keycloak.register()
+    }
+
         return (
             <div>
-                <div className='mt-2'>
-                    <h1>TCArgent HomePage</h1>
+                <tr>
+                    <td>
+                        <Drawer id="drawer">
+                            <div className='w-100'>
+                                <div className='mt-2 w-100'>
+                                    <ButtonPrimary icon="fa fa-user-circle-o" style={{width: "inherit"}} onClick={login}> Log In</ButtonPrimary>
+                                </div>
+                                <div className='mt-2 w-100'>
+                                    <ButtonPrimary icon="fa fa-sign-in" style={{width: "inherit"}} onClick={register}> Register </ButtonPrimary>
+                                </div>
+                                <div className='mt-2 w-100'>
+                                    <ButtonPrimary icon="fa fa-sign-out" style={{width: "inherit"}} onClick={logout}> Log Out</ButtonPrimary>
+                                </div>
+                            </div>
+                        </Drawer>
+
+                    </td>
+                    <td>
+                        <img
+                            src="TCArgent_Logo.png"
+                            alt=""
+                            title="Amazing TCArgent logo"
+                            width="100"
+                            height="100"></img>
+                    </td>
+                    <td>
+                        <h1>TCArgent</h1>
+                    </td>
+                </tr>
+                <tr>
+
+                </tr>
+                <div>
+                    <h2>Bienvenue sur TCArgent</h2>
                 </div>
-                <a href="https://localhost:3000/Sigin" target="_blanck">
-                    <ButtonPrimary name = "Sign In" color="primary" icon="fa fa-sign-in" />
-                </a>
-                <a href="https://localhost:3000/Login" target="_blanck">
-                    <ButtonPrimary color="primary" icon="fa fa-address-book" />
-                </a>
+                <div>
+                    <body>
+                        Nous sommes heureux de 
+                    </body>
+                </div>
             </div>
         )
-    }
 }
-
-export { HomePage }
