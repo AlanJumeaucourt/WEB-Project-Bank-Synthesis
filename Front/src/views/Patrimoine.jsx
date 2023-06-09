959126e9b0120288c5a2d2d298fad0e903fa13b0
alan jumeaucourt <alan.jumeaucourt@gmail.com>
2023-06-09 14:19:19 +0200

    Add card

--- Front/src/views/Patrimoine.jsx
+++ Front/src/views/Patrimoine.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { BasicLineChart, StackedAreaChart, SmoothedLineChart, SmoothedAreaChart, BasicAreaChart } from '../components/charts/Lines'
 import { useEffect, useState } from "react";
+import { Card } from '../components/ui_components/Containers';
 
 const Patrimoine = () => {
     const [priceData, setPriceData] = useState(null);
@@ -25,15 +26,56 @@ const Patrimoine = () => {
     return (
       <div>
         <h1>TCArgent HomePage</h1>
-        <div className='h-100' style={{ width: '100%' }}>
-            <div style={{ height: '1000px'}}>
-            {priceData ? (
-                <BasicLineChart title='Titre' xData={priceData.dates} name="legende" yData={priceData.priceUsd} color='red' />
-            ) : (
-                <p>Chargement des données...</p>
-            )}
-          </div>
+        <div style={{height:'1000px', width:'750px'}}>
+            <div className='h-50'>
+                    <BasicLineChart title='Titre' xData={['lun', 'mar', 'mer', 'jeu', 'ven']} name="legende" yData={[10, 50, 30, 60, 40]} color='red' />
+            </div>
         </div>
+
+        <Card class="card">
+            <div class="card-body">
+                Je suis une card
+            </div>
+        </Card>
+
+
+        <Card style={{ height:'500px'}}>
+            <div className='card h-50'>
+                <div class="card-body">
+                    This is some text within a card body.
+                </div>
+            </div>           
+        </Card>
+
+
+
+        <Card>
+        <div class="row">
+        <div class="col-sm-6 mb-3 mb-sm-0">
+            <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <div style={{height:'900px'}}>
+                    <div className='h-50'>
+                        <BasicLineChart title='Titre' xData={['lun', 'mar', 'mer', 'jeu', 'ven']} name="legende" yData={[10, 50, 30, 60, 40]} color='red' />
+                    </div>
+                </div>
+
+            </div>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
+        </div>
+        </div>
+        </Card>
       </div>
     );
   };
