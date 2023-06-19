1e7363d8fdfde507deba6e7d11f7184de700ed29
Gausse Dongmo <58121404+dkgmgo@users.noreply.github.com>
2023-06-19 00:42:38 +0000

    code cleaning on homepage

--- Front/src/views/HomePage.jsx
+++ Front/src/views/HomePage.jsx
@@ -1,28 +1,16 @@
-import React, { Children, Component } from 'react'
-import { HomeNavBar } from '../components/bars/NavBar'
+import React from 'react'
+import { SuccessMessage } from '../components/ui_components/StatusMessage'
 
 export function HomePage() {
     return (
         <div>
-            <div class="container">
+            <div className="container mt-2">
                 <header>
-                    <h1 style={{
-                        marginBottom: '20px',
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                    }}>
-                        Bienvenue sur notre site de gestion financière TCArgent</h1>
-
+                    <SuccessMessage title="Bienvenue !!" notDismissible={true}><p>{welcome}</p></SuccessMessage>
                 </header>
             </div>
 
-            <div class="container" style={{
-                marginBottom: '40px',
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-            }}>
+            <div className="container mb-5 d-flex justify-content-center align-items-center">
                 <img
                     src="Finance_picture.png"
                     title="Finance tool"
@@ -70,13 +58,15 @@ export function HomePage() {
                     </div>
                 </body>
 
-                <div class="container">
-                    <h2 style={{ marginTop: '20px' }}>Qui sommes nous et que proposons nous?</h2>
+                <div className="container">
+                    <h2 className='mt-4'>Qui sommes nous et que proposons nous?</h2>
                     <p>Nous sommes une start-up passionnée par la technologie financière, offrant un site web open source pour gérer facilement vos comptes bancaires et vos investissements. Notre plateforme conviviale et sécurisée vous permet de suivre vos transactions, consulter vos soldes et bien plus encore.</p>
-                    <p>Notre objectif est de rendre la gestion financière accessible à tous. Avec notre site web open source, nous encourageons la collaboration et l'évolution continue de nos fonctionnalités. Nous sommes fiers de fournir un produit de qualité, soutenu par une équipe talentueuse et soucieuse de votre satisfaction.</p>
+                    <p>Notre objectif est de rendre la gestion financière accessible à tous. Avec notre application web open source, nous encourageons la collaboration et l'évolution continue de nos fonctionnalités. Nous sommes fiers de fournir un produit de qualité, soutenu par une équipe talentueuse et soucieuse de votre satisfaction.</p>
                     <p>Rejoignez-nous dès aujourd'hui et prenez le contrôle de vos finances avec TCArgent. Simplifiez votre vie financière et contribuez à un écosystème financier plus ouvert et transparent.</p>
                 </div>
             </div>
         </div >
     )
 }
+
+const welcome = "Nous sommes ravis de vous accueillir parmi notre communauté grandissante d'utilisateurs dédiés à la maîtrise de leurs finances. Que vous soyez un novice en matière de gestion financière ou un expert chevronné, notre plateforme est conçue pour vous aider à atteindre vos objectifs financiers. Avec notre site, vous pourrez suivre vos revenus, dépenses, investissements et budgets en un seul endroit pratique.Nous mettons à votre disposition des outils conviviaux et intuitifs pour vous aider à visualiser et à analyser vos données financières, vous permettant ainsi de prendre des décisions éclairées en toute confiance."
