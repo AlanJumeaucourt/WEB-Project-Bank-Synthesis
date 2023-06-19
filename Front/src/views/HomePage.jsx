f2d1473673b14e7c7371c3013cae3ace5df1f5ba
Ryad3 <ryadharittane@live.fr>
2023-06-19 18:30:11 +0200

    Changement HomePage et AboutUs

--- Front/src/views/HomePage.jsx
+++ Front/src/views/HomePage.jsx
@@ -21,18 +21,20 @@ export function HomePage() {
                 <body>
                     <div id='Ryad' className="container" style={{ marginBottom: '30px' }}>
                         <div className="bubble">
-                            <h3>Gestion des comptes bancaires des utilisateurs</h3>
-                            <p>Description de la fonctionnalité...</p>
+                            <h4>Gestion des comptes bancaires</h4>
+                            <p>Ayez une vision globales de vos comptes bancaires même s'ils ne sont pas dans la même banque.</p>
                         </div>
 
                         <div className="bubble">
-                            <h3>Monitoring des comptes et patrimoines</h3>
-                            <p>Description de la fonctionnalité...</p>
+                            <h4>Monitoring de votre patrimoine</h4>
+                            <p></p>
+                            <p>En plus de gérer vos comptes, visualisé votre patrimoine globales.</p>
                         </div>
 
                         <div className="bubble">
-                            <h3>Gestion des prêts</h3>
-                            <p>Description de la fonctionnalité...</p>
+                            <h4>Gestion des investissements</h4>
+                            <p></p>
+                            <p>Ayez une vision globale et de manière durable de vos différents investissements.</p>
                         </div>
 
                         <style>
@@ -58,12 +60,6 @@ export function HomePage() {
                     </div>
                 </body>
 
-                <div className="container">
-                    <h2 className='mt-4'>Qui sommes nous et que proposons nous?</h2>
-                    <p>Nous sommes une start-up passionnée par la technologie financière, offrant un site web open source pour gérer facilement vos comptes bancaires et vos investissements. Notre plateforme conviviale et sécurisée vous permet de suivre vos transactions, consulter vos soldes et bien plus encore.</p>
-                    <p>Notre objectif est de rendre la gestion financière accessible à tous. Avec notre application web open source, nous encourageons la collaboration et l'évolution continue de nos fonctionnalités. Nous sommes fiers de fournir un produit de qualité, soutenu par une équipe talentueuse et soucieuse de votre satisfaction.</p>
-                    <p>Rejoignez-nous dès aujourd'hui et prenez le contrôle de vos finances avec TCArgent. Simplifiez votre vie financière et contribuez à un écosystème financier plus ouvert et transparent.</p>
-                </div>
             </div>
         </div >
     )
