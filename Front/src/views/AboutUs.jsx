aecdf7e94c7c1b1651088447f3c2c541ca0f16ef
Ryad3 <ryadharittane@live.fr>
2023-06-19 19:31:02 +0200

    Dernier ajustement du AboutUs + régler problème appel fct

--- Front/src/views/AboutUs.jsx
+++ Front/src/views/AboutUs.jsx
@@ -1,24 +1,22 @@
 import React from 'react'
 
-export function HomePage() {
+export function AboutUs() {
     return (
         <div>
-            <div class="container">
-                <h2>Qui sommes nous et que proposons nous?</h2>
+            <div class="container" style={{ marginBottom: '30px' }}>
+                <h2 style={{ marginBottom: '20px' }}>Qui sommes nous et que proposons nous?</h2>
                 <p>Nous sommes une start-up passionnée par la technologie financière, offrant un site web open source pour gérer facilement vos comptes bancaires et vos investissements. Notre plateforme conviviale et sécurisée vous permet de suivre vos transactions, consulter vos soldes et bien plus encore.</p>
                 <p>Notre objectif est de rendre la gestion financière accessible à tous. Avec notre site web open source, nous encourageons la collaboration et l'évolution continue de nos fonctionnalités. Nous sommes fiers de fournir un produit de qualité, soutenu par une équipe talentueuse et soucieuse de votre satisfaction.</p>
                 <p>Rejoignez-nous dès aujourd'hui et prenez le contrôle de vos finances avec TCArgent. Simplifiez votre vie financière et contribuez à un écosystème financier plus ouvert et transparent.</p>
             </div>
             <div class="container">
-                <h2>Co-Founder du projet :</h2>
-                <ul>
-                    <li>Harittane Ryadou <a href="https://github.com/Ryad3">@Ryad3</a></li>
-                    <li>Jumeaucourt Dictateur <a href="https://github.com/AlanJumeaucourt">@AlanJumeaucourt</a></li>
-                    <li>Landy Lulu <a href="https://github.com/Zouzzou21">@Zouzzou21</a></li>
-                    <li>Beaufour Mathou <a href="https://github.com/CABANENT">@CABANENT</a></li>
-                    <li>Cambray L'homo <a href="https://github.com/Moovaa2">@Moovaa2</a></li>
-                    <li>Dongmo la MACHINE <a href="https://github.com/dkgmgo">@dkgmgo</a></li>
-                </ul>
+                <h2 style={{ marginBottom: '20px' }}>Co-Founder du projet :</h2>
+                    <p>Harittane Ryadou <a href="https://github.com/Ryad3">@Ryad3</a></p>
+                    <p>Jumeaucourt Dictateur <a href="https://github.com/AlanJumeaucourt">@AlanJumeaucourt</a></p>
+                    <p>Landy Lulu <a href="https://github.com/Zouzzou21">@Zouzzou21</a></p>
+                    <p>Beaufour Mathou <a href="https://github.com/CABANENT">@CABANENT</a></p>
+                    <p>Cambray L'homo <a href="https://github.com/Moovaa2">@Moovaa2</a></p>
+                    <p>Dongmo la MACHINE <a href="https://github.com/dkgmgo">@dkgmgo</a></p>
             </div>
         </div>
     )
